88aac4b8ba8b242e8baf50b246b4e3ef7d2951d3
oscar <o.veigam@gmail.com>
2024-11-21 18:51:04 +0100

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: HomePage,
+});
+
+function HomePage() {
+  return (
+    <main>
+      <h1>Hello pokemon</h1>
+    </main>
+  );
+}
