726d2e6d4d4fc0c5ed1237f9d67d197f8c5a1a33
oscar <o.veigam@gmail.com>
2025-01-08 09:49:59 +0100

    visible column resizer

--- app/lib/datagrid/features/size/components/col-resizer.tsx
+++ app/lib/datagrid/features/size/components/col-resizer.tsx
@@ -1,6 +1,7 @@
 import { useDndContext } from "@dnd-kit/core";
 import type { Header } from "@tanstack/react-table";
 
+import { Separator } from "@ui/components/core/separator";
 import { cn } from "@ui/util/class-name";
 
 export function ColumnResizer<TData>({ header }: { header: Header<TData, unknown> }) {
@@ -9,9 +10,8 @@ export function ColumnResizer<TData>({ header }: { header: Header<TData, unknown
   return (
     <div
       className={cn(
-        "pointer absolute bottom-0 right-0 top-0 flex w-3 cursor-ew-resize touch-none select-none justify-end opacity-0 hover:opacity-100",
+        "pointer group/resizer absolute bottom-0 right-0 top-0 flex w-3 cursor-ew-resize touch-none select-none justify-end",
         {
-          "opacity-100": header.column.getIsResizing(),
           hidden: !!ctx.active,
         },
       )}
@@ -19,7 +19,12 @@ export function ColumnResizer<TData>({ header }: { header: Header<TData, unknown
       onMouseDown={header.getResizeHandler()}
       onTouchStart={header.getResizeHandler()}
     >
-      <div className="h-full w-0.5 bg-primary" />
+      <Separator
+        orientation="vertical"
+        className={cn("h-full group-hover/resizer:w-0.5 group-hover/resizer:bg-primary", {
+          "w-0.5 bg-primary": header.column.getIsResizing(),
+        })}
+      />
     </div>
   );
 }
